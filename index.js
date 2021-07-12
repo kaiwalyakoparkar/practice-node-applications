1c28885716fd7e22f6d1b29ab745528b0005bdf1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-07-12 19:22:21 +0200

    fix: add bodyparser

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
 const port = 3000;
 
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', require(path.join(__dirname, './routes/server.js')));
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+app.use("/", require(path.join(__dirname, "./routes/server.js")));
 
 app.listen(port, () => {
-    console.log(`Server started at : http://localhost:${port}`);
+  console.log(`Server started at : http://localhost:${port}`);
 });
