fb7ea6ab3386d0b5480a264f57018cbf54162b4f
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-08-25 12:09:45 +0530

    feat: :art: Linters and prettiers adeed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 if(process.env.ENVIRONMENT == 'developement'){
 	app.use(morgan('dev'));
 }
+app.use(express.json());
 
 app.use('/api/v1/blogs', require(path.join(__dirname, './routes/blogRoute.js')));
 
