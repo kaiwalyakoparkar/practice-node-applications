706bc9c1c4279be8f90b22053465de4ced5d2fcd
Eddie Jaoude <eddie@jaoudestudios.com>
2021-08-26 01:14:24 +0100

    fix: mongo service envars (#4)

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const app = require(path.join(__dirname,'./index.js'));
+const app = require(path.join(__dirname, './index.js'));
 
-const port = process.env.PORT;
-const DB = process.env.DATABASE_CONNECTION_STRING.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const PORT = process.env.PORT || 3000;
+const DB = process.env.DATABASE_CONNECTION_STRING.replace(
+	'<PASSWORD>',
+	process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-}).then(() => {
-	console.log('MongoDB connected Successfullly 🔗🔒');
-});
-
-app.listen(port, () => {
-	console.log(`Server started on port ${port}`)
-});
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB connected Successfullly 🔗🔒'))
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+	);
